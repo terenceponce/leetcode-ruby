b0a97ec77f97d8d7a73b642eae4b8ef6c9471e3d
Terence Ponce <terenceponce@gmail.com>
2023-12-09 20:49:55 +0000

    Add Rubocop for Linting/Formatting
    This installs Rubocop and integrates it into the CI
    
    This also fixes some of the errors found by Rubocop

--- test/solutions/0001_two_sum/two_sum_test.rb
+++ test/solutions/0001_two_sum/two_sum_test.rb
@@ -5,14 +5,14 @@ require_relative '../../../lib/solutions/0001_two_sum/two_sum'
 
 class TwoSumTest < Minitest::Test
   def test_case_1_works
-    assert_equal two_sum([2, 7, 11, 15], 9), [0, 1]
+    assert_equal [0, 1], two_sum([2, 7, 11, 15], 9)
   end
 
   def test_case_2_works
-    assert_equal two_sum([3, 2, 4], 6), [1, 2]
+    assert_equal [1, 2], two_sum([3, 2, 4], 6)
   end
 
   def test_case_3_works
-    assert_equal two_sum([3, 3], 6), [0, 1]
+    assert_equal [0, 1], two_sum([3, 3], 6)
   end
 end
